d7d93236e06ec8176b4ce9554e8cb81fa6632e7a
Nikolai Shegai <nixer1978@yahoo.com>
2014-04-22 22:58:46 -0700

    Update HtmlUnit_CSV_try.java

--- src/main/java/core/Title_Validation_CSV/HtmlUnit_CSV_try.java
+++ src/main/java/core/Title_Validation_CSV/HtmlUnit_CSV_try.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 public class HtmlUnit_CSV_try {
 	public static void main(String[] args) {
 
-		String csvFile = "./src/main/resources/Test.csv";
+		String csvFile = "../src/main/resources/Test.csv";
 		BufferedReader br = null;
 		String line = null;
 		String cvsSplitBy = ",";
